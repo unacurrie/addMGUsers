545cb8afe175ba0d1b0a4641daf3039afe407aca
Lee Newton <lgnewton@gmail.com>
2023-01-21 08:54:08 +0000

    moved pw creation untill after name check is completed as to not run function needlessly and added inline comment to explain use of continue keyword

--- CreateAADUsers.ps1
+++ CreateAADUsers.ps1
@@ -1,6 +1,5 @@
 # start from new connection and if no connection exists just SilentlyContinue
 Disconnect-MgGraph -ErrorAction SilentlyContinue
-
 Connect-MgGraph
 
 function createPW {
@@ -10,17 +9,17 @@ function createPW {
 }
 
 function createUsers {
-
   $mgusers = Get-MgUser
   $users = Get-Content .\data\users.json | ConvertFrom-Json
 
   foreach ($user in $users) {
-    $userPW = createPW
     $mgusers | ForEach-Object {
       if ($user.name -match $_.DisplayName) {
+        #perosnal note here this continue is not for the ForEach-Object loop as this is a script block. This continue will break out of this current forEach loop and move to the next iteration. Important to understand how continue is working here!  
         continue
       }
     }
+    $userPW = createPW
     $username = ($user.name.Replace(' ', ''))
     $firstName = ($user.name -split ' ', 0)[0]
     New-MgUser `
@@ -33,5 +32,4 @@ function createUsers {
       -UsageLocation GB
   }
 }
-
 createUsers
